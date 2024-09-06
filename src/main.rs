2c9386673641911309c734645f0da6522bb26a5c
Huy Nguyen <huy55465@gmail.com>
2024-09-06 18:58:14 -0400

    use nonlock printing instead
    cuz im on a single thread. for now.

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,56 @@ fn main() -> io::Result<()> {
         }
     };
 
-    let stdin = io::stdin();
-    while let Some(Ok(input)) = stdin.lock().lines().next() {
-        if let Some(match_substr) = rare.match_all_index(&input) {
+    // lock version
+    // let stdin = io::stdin();
+    // while let Some(Ok(input)) = stdin.lock().lines().next() {
+    //     if let Some(match_substr) = rare.match_all(&input) {
+    //         let mut prev_last_idx = 0;
+    //         for substr_range in match_substr.iter() {
+    //             // println!("{}, {}", substr_range.0, substr_range.1);
+    //             if let Some(out) = get_substring(&input, substr_range.0, substr_range.1) {
+    //                 // print!("{out}");
+    //                 print!("{}", &input[prev_last_idx..substr_range.0]);
+    //                 print!("\x1b[31m{out}\x1b[0m");
+    //                 prev_last_idx = substr_range.1;
+    //             }
+    //         }
+    //         println!("\x1b[0m{}", &input[prev_last_idx..]);
+    //     }
+    // }
+
+    // nonlock version
+    let in_handle = io::stdin().lock();
+    for buf_result in in_handle.lines() {
+        let buf = if let Ok(ret) = buf_result {
+            ret
+        } else {
+            continue;
+        };
+        if let Some(match_substr) = rare.match_all(&buf) {
             let mut prev_last_idx = 0;
-            for substr_range in &match_substr {
-                print!(
-                    "{0}\x1b[31m\x1b[1m{1}\x1b[0m",
-                    &input[prev_last_idx..substr_range.0],
-                    &input[substr_range.0..substr_range.1]
-                );
-                prev_last_idx = substr_range.1;
-                // println!("{0}, {1}", substr_range.0, substr_range.1);
+            for substr_range in match_substr.iter() {
+                if let Some(out) = get_substring(&buf, substr_range.0, substr_range.1) {
+                    print!("{}", &buf[prev_last_idx..substr_range.0]);
+                    print!("\x1b[31m{out}\x1b[0m");
+                    prev_last_idx = substr_range.1;
+                }
+                // println!("{}, {}", substr_range.0, substr_range.1);
             }
-            print!("{0}", &input[prev_last_idx..]);
-            println!();
+            println!("{}", &buf[prev_last_idx..]);
         }
     }
 
     Ok(())
+}
+
+#[inline]
+fn get_substring(input: &str, beg: usize, end: usize) -> Option<&str> {
+    if beg >= input.len() {
+        None
+    } else if end >= input.len() {
+        Some(&input[beg..])
+    } else {
+        Some(&input[beg..end])
+    }
 }
