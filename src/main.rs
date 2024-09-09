f962b3408dfbea26381c9ffb73b2f6b7948422a0
Huy Nguyen <huy55465@gmail.com>
2024-09-09 12:09:59 -0400

    no more fancy stuff
    but performance improves quite significantly in most cases. Again,
    "most"
    
    At least it's way more practical now.
    
    commit-by: Huy Nguyen

--- src/main.rs
+++ src/main.rs
@@ -59,49 +59,50 @@ fn main() -> io::Result<()> {
     //     }
     // }
 
-    let in_handle = io::stdin().lock();
-    let mut out_lock = io::stdout().lock();
-    let mut buf_vec: Vec<(usize, usize)> = Vec::new();
-    for buf_result in in_handle.lines() {
-        let buf = if let Ok(ret) = buf_result {
-            ret
-        } else {
-            continue;
-        };
-        rare.write_match_all(&buf, &mut buf_vec);
-        let mut prev_last_idx = 0;
-        if buf_vec.is_empty() {
-            continue;
-        }
-        for substr_range in buf_vec.drain(..) {
-            if let Some(out) = get_substring(&buf, substr_range.0, substr_range.1) {
-                // print!("{}", &buf[prev_last_idx..substr_range.0]);
-                // print!("\x1b[31m{out}\x1b[0m");
-                write!(out_lock, "{}\x1b[31m{out}\x1b[0m", &buf[prev_last_idx..substr_range.0])?;
-                prev_last_idx = substr_range.1;
-            }
-            // println!("{}, {}", substr_range.0, substr_range.1);
-        }
-        println!("{}", &buf[prev_last_idx..]);
-    }
-
-    // is match or not only. No highlight.
-    // scales way better. Although still smoked by grep.
     // let in_handle = io::stdin().lock();
+    // let mut out_lock = io::stdout().lock();
+    // let mut buf_vec: Vec<(usize, usize)> = Vec::new();
     // for buf_result in in_handle.lines() {
     //     let buf = if let Ok(ret) = buf_result {
     //         ret
     //     } else {
     //         continue;
     //     };
-    //     if rare.is_match(&buf) {
-    //         println!("{buf}");
+    //     rare.write_match_all(&buf, &mut buf_vec);
+    //     let mut prev_last_idx = 0;
+    //     if buf_vec.is_empty() {
+    //         continue;
     //     }
+    //     for substr_range in buf_vec.drain(..) {
+    //         if let Some(out) = get_substring(&buf, substr_range.0, substr_range.1) {
+    //             // print!("{}", &buf[prev_last_idx..substr_range.0]);
+    //             // print!("\x1b[31m{out}\x1b[0m");
+    //             write!(out_lock, "{}\x1b[31m{out}\x1b[0m", &buf[prev_last_idx..substr_range.0])?;
+    //             prev_last_idx = substr_range.1;
+    //         }
+    //         // println!("{}, {}", substr_range.0, substr_range.1);
+    //     }
+    //     println!("{}", &buf[prev_last_idx..]);
     // }
+
+    // is match or not only. No highlight.
+    // scales way better. Although still smoked by grep.
+    let in_handle = io::stdin().lock();
+    for buf_result in in_handle.lines() {
+        let buf = if let Ok(ret) = buf_result {
+            ret
+        } else {
+            continue;
+        };
+        if rare.is_match(&buf) {
+            println!("{buf}");
+        }
+    }
 
     Ok(())
 }
 
+#[allow(dead_code)]
 #[inline]
 fn get_substring(input: &str, beg: usize, end: usize) -> Option<&str> {
     if beg >= input.len() {
