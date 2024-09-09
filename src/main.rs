e136f879f975bede8b469c017b2d7a81bc76e5be
Huy Nguyen <huy55465@gmail.com>
2024-09-09 11:09:23 -0400

    add a bunch of possible main() calls
    useful? maybe?
    
    commit-by: Huy Nguyen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{self, BufRead},
+    io::{self, BufRead, Write},
     process::exit,
 };
 
@@ -38,26 +38,66 @@ fn main() -> io::Result<()> {
     // }
 
     // nonlock version
+    // let in_handle = io::stdin().lock();
+    // for buf_result in in_handle.lines() {
+    //     let buf = if let Ok(ret) = buf_result {
+    //         ret
+    //     } else {
+    //         continue;
+    //     };
+    //     if let Some(match_substr) = rare.match_all(&buf) {
+    //         let mut prev_last_idx = 0;
+    //         for substr_range in match_substr.iter() {
+    //             if let Some(out) = get_substring(&buf, substr_range.0, substr_range.1) {
+    //                 print!("{}", &buf[prev_last_idx..substr_range.0]);
+    //                 print!("\x1b[31m{out}\x1b[0m");
+    //                 prev_last_idx = substr_range.1;
+    //             }
+    //             // println!("{}, {}", substr_range.0, substr_range.1);
+    //         }
+    //         println!("{}", &buf[prev_last_idx..]);
+    //     }
+    // }
+
     let in_handle = io::stdin().lock();
+    let mut out_lock = io::stdout().lock();
+    let mut buf_vec: Vec<(usize, usize)> = Vec::new();
     for buf_result in in_handle.lines() {
         let buf = if let Ok(ret) = buf_result {
             ret
         } else {
             continue;
         };
-        if let Some(match_substr) = rare.match_all(&buf) {
-            let mut prev_last_idx = 0;
-            for substr_range in match_substr.iter() {
-                if let Some(out) = get_substring(&buf, substr_range.0, substr_range.1) {
-                    print!("{}", &buf[prev_last_idx..substr_range.0]);
-                    print!("\x1b[31m{out}\x1b[0m");
-                    prev_last_idx = substr_range.1;
-                }
-                // println!("{}, {}", substr_range.0, substr_range.1);
-            }
-            println!("{}", &buf[prev_last_idx..]);
+        rare.write_match_all(&buf, &mut buf_vec);
+        let mut prev_last_idx = 0;
+        if buf_vec.is_empty() {
+            continue;
         }
+        for substr_range in buf_vec.drain(..) {
+            if let Some(out) = get_substring(&buf, substr_range.0, substr_range.1) {
+                // print!("{}", &buf[prev_last_idx..substr_range.0]);
+                // print!("\x1b[31m{out}\x1b[0m");
+                write!(out_lock, "{}\x1b[31m{out}\x1b[0m", &buf[prev_last_idx..substr_range.0])?;
+                prev_last_idx = substr_range.1;
+            }
+            // println!("{}, {}", substr_range.0, substr_range.1);
+        }
+        println!("{}", &buf[prev_last_idx..]);
     }
+
+    // is match or not only. No highlight.
+    // scales way better. Although still smoked by grep.
+    // let in_handle = io::stdin().lock();
+    // for buf_result in in_handle.lines() {
+    //     let buf = if let Ok(ret) = buf_result {
+    //         ret
+    //     } else {
+    //         continue;
+    //     };
+    //     if rare.is_match(&buf) {
+    //         println!("{buf}");
+    //     }
+    // }
 
     Ok(())
 }
